b97972a15497e3bf38e56e5ee79aba645394e836
gabriel88-aagem <gabrielmolinagoularte@gmail.com>
2026-06-01 13:43:32 -0300

    a
    a

---
+++ tirinha.js
@@ -0,0 +1,42 @@
+const JOGOS = [
+  "Sonho",
+  "Cavar",
+  "Panqueca",
+  "Jogar cattoverse parte 1",
+  "Jogar cattoverse parte 2",
+  "Ritual de Beatbox",
+  "Maluco",
+  "Nightcore Desktop parte 1",
+  "Nightcore Desktop parte 2",
+  "88",
+  "Armadilha",
+  "Boca",
+  "Botão na praia",
+  "Matemática",
+  "Móveis enbocanhadores",
+  "Escola gritante parte 1",
+  "Escola gritante parte 2",
+  "Basquete"
+];
+
+// Embedding Codigo.txt contents so the site works without a server
+window.CODIGOS = {
+  "Sonho": "1202261TO",
+  "Cavar": "1302261TO",
+  "Panqueca": "2002261TO",
+  "Jogar cattoverse parte 1": "2402261TO",
+  "Jogar cattoverse parte 2": "2402261TO",
+  "Ritual de Beatbox": "2602261TO",
+  "Maluco": "2702261TO",
+  "Nightcore Desktop parte 1": "0403261TO",
+  "Nightcore Desktop parte 2": "0403261TO",
+  "88": "1603261TO",
+  "Armadilha": "2603261TO",
+  "Boca": "1604261TO",
+  "Botão na praia": "2304261TO",
+  "Matemática": "0605261TO",
+  "Móveis enbocanhadores": "0705261TO",
+  "Escola gritante parte 1": "1305261TO",
+  "Escola gritante parte 2": "1305261TO",
+  "Basquete": "1405261TO"
+};
