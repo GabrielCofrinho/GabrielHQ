0406451a5421e991de8920db494a1a0a6ad38a67
gabriel88-aagem <gabrielmolinagoularte@gmail.com>
2026-07-30 22:24:14 -0300

    Nova IA!
    Super IA que deixa você conhecer melhor!

---
+++ sons.js
@@ -0,0 +1,12 @@
+// sons.js - Cadastro manual dos nomes dos arquivos de áudio
+const SONS_IA = [
+    "IAfala1.wav",
+    "IAfala2.wav",
+    "IAfala3.wav"
+];
+
+const SONS_USER = [
+    "USERfala1.wav",
+    "USERfala2.wav",
+    "USERfala3.wav"
+];
